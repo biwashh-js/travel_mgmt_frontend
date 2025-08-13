64449d75b12d4b9b95a36fdd83e4829a80c7e308
biwash-js <parajulibibas81@gmail.com>
2025-08-13 17:56:06 +0545

    login page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+import Login from './pages/login'
+const App = () => {
+  return (
+    <div className='h-full w-full'>
+      <Login/>
+    </div>
+  )
+}
+
+export default App
