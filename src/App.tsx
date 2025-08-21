741fcc022b153a705fbecb0885786ea1e51494ce
biwash-js <parajulibibas81@gmail.com>
2025-08-21 18:10:15 +0545

    test router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 
