2cde9ba3fe97556b79f2d3921ab59019fa90f7b7
biwash-js <parajulibibas81@gmail.com>
2025-08-22 10:51:33 +0545

    home page with login and sign up

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,28 @@
+import { Link } from "react-router"
 
 
 const Home = () => {
   return (
     <div>
       home page
+       <div className="flex items-center justify-center text-[12px] mt-5">
+            <p>
+              <Link to ={'/sign-up'}>
+              <span className="text-blue-700">
+                Sign Up
+              </span>
+              </Link>
+            </p>
+        </div>
+         <div className="flex items-center justify-center text-[12px] mt-5">
+          <p>
+            <Link to ={'/login'}>
+            <span className="text-blue-700">
+              Login
+            </span>
+            </Link>
+          </p>
+        </div>
     </div>
   )
 }
