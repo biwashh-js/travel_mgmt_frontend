eb4eb7e2ac72bbc4c286810450dfe04c620eefcf
biwash-js <parajulibibas81@gmail.com>
2025-08-22 10:46:47 +0545

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
+import Home from "./pages/home";
 
 
 
@@ -12,7 +13,7 @@ const App = () => {
     <main className="h-full    w-full">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<div>Home Page</div>}/>
+          <Route path='/' element={<Home/>}/>
           <Route path="/login" element={<Login/>} />
           <Route path='/sign-up' element={<Signup/>} />
           <Route path='*' element={<div>Page not found</div>}/>
